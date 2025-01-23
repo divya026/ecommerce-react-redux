bf389f307b5cd183f47ded32c0fe2601043a00c4
Divya <divya.ttl@tatamotors.com>
2025-01-23 11:52:51 +0530

    categories-section-added

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import "./Categories.css";
+import { categories } from "../../data";
+import PrimaryButton from "../button/PrimaryButton";
+import category1 from "../../assets/category1.jpg";
 
 const Categories = () => {
-  return <div>Categories</div>;
+  return (
+    <div className="categories-container">
+      {categories.map((i) => (
+        <div key={i.id} className="category-container">
+          <img src={i.img}></img>
+          <div className="category-info">
+            <h1>{i.title} </h1>
+            <PrimaryButton name="Shop Now" />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Categories;
